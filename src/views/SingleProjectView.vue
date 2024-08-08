5d5556e9ca8163aab8e653c3298730ab9802e506
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-07 23:11:12 -0300

    feat: reading route params id

--- src/views/SingleProjectView.vue
+++ src/views/SingleProjectView.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
 
 <template>
   <div>
-    <h1>Project Name</h1>
+    <h1>Project {{ route.params?.id }}</h1>
   </div>
 </template>
