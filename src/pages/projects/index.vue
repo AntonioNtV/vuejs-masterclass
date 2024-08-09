88b07a04fd3cdf28325e69d261df58fcbbe41468
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-09 08:52:44 -0300

    feat: navigate using table

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -4,16 +4,15 @@ import { h, ref } from 'vue'
 import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 import DataTable from '@/components/ui/data-table/DataTable.vue'
+import { RouterLink } from 'vue-router'
 
 const projects = ref<Tables<'projects'>[] | null>(null)
 ;(async () => {
   const { data, error } = await supabase.from('projects').select()
 
-  if (error) {
-    console.log(error)
-  }
+  if (error) console.log(error)
+
   projects.value = data
-  console.log(projects)
 })()
 
 const columns: ColumnDef<Tables<'projects'>>[] = [
@@ -21,14 +20,21 @@ const columns: ColumnDef<Tables<'projects'>>[] = [
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('name'))
+      return h(
+        RouterLink,
+        {
+          to: `/projects/${row.original.slug}`,
+          class: 'text-left font-medium hover:bg-muted block w-full'
+        },
+        () => row.original.name
+      )
     }
   },
   {
     accessorKey: 'status',
     header: () => h('div', { class: 'text-left' }, 'Status'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left font-medium' }, row.getValue('status'))
+      return h('div', { to: '', class: 'text-left font-medium' }, row.original.status)
     }
   },
   {
@@ -37,8 +43,8 @@ const columns: ColumnDef<Tables<'projects'>>[] = [
     cell: ({ row }) => {
       return h(
         'div',
-        { class: 'text-left font-medium' },
-        JSON.stringify(row.getValue('collaborators'))
+        { to: '', lass: 'text-left font-medium' },
+        JSON.stringify(row.original.collaborators)
       )
     }
   }
