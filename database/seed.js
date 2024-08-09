d8ee0fdd6bc3ae228921cb6aa32f8396cd068ddb
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-08 22:31:39 -0300

    feat: bulk seed projects table

--- database/seed.js
+++ database/seed.js
@@ -4,3 +4,22 @@ import { fakerEN_US as faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
+
+const seedProjects = async (numEntries) => {
+  const projects = []
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+    const slug = name.toLocaleLowerCase().replace(/ /g, '-')
+
+    projects.push({
+      name: name,
+      slug: slug,
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
+    })
+  }
+
+  await supabase.from('projects').insert(projects)
+}
+
+await seedProjects(10)
