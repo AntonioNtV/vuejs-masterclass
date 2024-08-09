c842ec55f0b82ea5be9992c0854b9425e6653873
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-08 21:33:49 -0300

    feat: project env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://emivktfxungjieuwvoox.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtaXZrdGZ4dW5namlldXd2b294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxNjE1NjQsImV4cCI6MjAzODczNzU2NH0.ciaEkSiQCf6WzEh-yDyK8DUnW46HY8csyvvvX7M96mw'
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
