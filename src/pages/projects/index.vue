a6112ff7275f145d40732e6778b23e484e6bf880
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2024-08-10 19:08:00 -0300

    feat: cleanup template

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,58 +1,7 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-import type { Tables } from '../../../database/types'
-import { RouterLink } from 'vue-router'
-import type { ColumnDef } from '@tanstack/vue-table'
-
 usePageStore().pageData.title = 'Projects'
-
-const projects = ref<Tables<'projects'>[] | null>(null)
-const getProjects = async () => {
-  const { data, error } = await supabase.from('projects').select()
-
-  if (error) console.log(error)
-
-  projects.value = data
-}
-
-await getProjects()
-
-const columns: ColumnDef<Tables<'projects'>>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.original.name
-      )
-    }
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => {
-      return h('div', { to: '', class: 'text-left font-medium' }, row.original.status)
-    }
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => {
-      return h(
-        'div',
-        { to: '', lass: 'text-left font-medium' },
-        JSON.stringify(row.original.collaborators)
-      )
-    }
-  }
-]
 </script>
 
 <template>
-  <DataTable v-if="projects" :columns="columns" :data="projects" />
+  <h1>Projects</h1>
 </template>
